cc62ec6bc3d4321865a4ae961890594c76405b60
AhmedAtef <90651336+AhmedAtef283@users.noreply.github.com>
2022-04-03 01:57:08 +0200

    Update ClockScreen.dart

--- lib/modules/tabs/ClockScreen.dart
+++ lib/modules/tabs/ClockScreen.dart
@@ -1,49 +1,29 @@
-import 'package:first_app/modules/tabs/AlarmScreen.dart';
-import 'package:first_app/modules/tabs/ClockScreen.dart';
-import 'package:first_app/modules/tabs/StopWatchScreen.dart';
-import 'package:first_app/modules/tabs/TimerScreen.dart';
 import 'package:flutter/material.dart';
 
-class ClockApp extends StatelessWidget {
-
-
+class ClockScreen extends StatefulWidget {
+  const ClockScreen({Key key}) : super(key: key);
 
   @override
+  State<ClockScreen> createState() => _ClockScreenState();
+}
+
+class _ClockScreenState extends State<ClockScreen> {
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController( length: 4,
-      child: Scaffold(
-      appBar: AppBar(backgroundColor: Colors.blueGrey,
-          title: Text('Clock App'),centerTitle: true, shadowColor: Colors.amber, elevation: 1,
-      bottom:TabBar(
+    return Center(child:
+         Column(mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             Text(
+               DateTime.now().hour  > 12 ? "Good Evening" : 'Good Morning' ,
+               style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),
+             ),
+             SizedBox(height: 10.0,) ,
+             Text(
+                 DateTime.now().hour > 12 ? '${DateTime.now().hour-12} :${DateTime.now().minute} PM ' : '${DateTime.now().hour-12} :${DateTime.now().minute} AM  ' ,
+               style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0,color: Colors.black45),),
 
-        tabs: [
-          Tab(child:Text('Clock',style:TextStyle(color: Colors.amber)),
-            icon:  Image.asset('images/clock.png',height: 30.0),),
-          Tab(child:Text('Alarm',style:TextStyle(color: Colors.amber)),
-              icon:  Image.asset('images/external-alarm-back-to-school-fauzidea-blue-fauzidea.png',height: 30.0)),
-          Tab(child:Text('Timer',style:TextStyle(color: Colors.amber)),
-              icon: Image.asset('images/timer.png',height: 30.0)),
-          Tab(child:
-          Center(child: Text('Stop Watch',style:TextStyle(color: Colors.amber),maxLines: 2,)),
-            icon:  Image.asset('images/external-stopwatch-fitness-and-gym-justicon-lineal-justicon.png',height: 30.0),),
+         ],)
+ );
 
-
-
-        ],
-      ), ),
-
-      body: TabBarView(
-        children: [
-          ClockScreen(),
-          AlarmScreen(),
-          TimerScreen(),
-          StopWatchScreen(),
-
-        ],
-      ),
-
-
-    )
-  );
   }
 }
